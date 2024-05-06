64888875d457eaf63e228bcdcdfca95e1b7f1d6f
Alex19901509 <147518422+Alex19901509@users.noreply.github.com>
2024-05-06 14:29:15 +0300

    finish

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pprint import pprint
 
+from src.getting_and_working_with_vacancies import HHAPI
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from src.add_vacancies_in_json import WorkWithJSON
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def hello():
+    # Обращения к пользователю
+    profession = input("Введите профессию для поиска ")
+    quantity_profession = int(input("Какое количество вакансий с наибольшей зарплатой вы хотите увидеть?"))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Указываю ссылку на API HH
+    hh_url = HHAPI('https://api.hh.ru/vacancies')
+
+    # Получаю вакансии и сортирую их
+    vacancies = hh_url.get_info(profession)
+    vacancies_sorted = sorted(vacancies, reverse=True)
+
+    # Записываю все вакансии в JSON файл
+    WorkWithJSON.add_json(vacancies)
+
+    # Спрашиваю у пользователя вывести ли топ вакансий в количестве которое он указал?
+    answer_1 = input('Хотите увидеть вакансии с наибольшей зарплатой?')
+    if answer_1 == 'да':
+        # Вывожу отсортированные вакансии в том количестве которое попросил пользователь
+        pprint(vacancies_sorted[:quantity_profession])
+
+    # Спрашиваю у пользователя хочет ли он отсортировать вакансии
+    answer_2 = input('Отсортируем вакансии?')
+    if answer_2 == 'да':
+        # Спрашиваю у пользователя по чему будем сортировать
+        answer_3 = input('Сортируем по названию или по требованиям?')
+        if answer_3 == 'по названию':
+            # Спрашиваю у пользователя название вакансии для сортировки
+            answer_4 = input('Введите ключевое слово в названии вакансии')
+            WorkWithJSON.get_info_json_name(answer_4)
+        elif answer_3 == 'по требованиям':
+            answer_5 = input('Введите ключевое слово в описании вакансии')
+            WorkWithJSON.get_info_json_requirements(answer_5)
+    answer_6 = input('Введите диапазон зарплат через -(10000-100000)')
+    answer_6_split = answer_6.split('-')
+    for salary in WorkWithJSON.filtered_information:
+        if int(salary['salary_from']) > int(answer_6_split[0]):
+            print(f'{salary["name"]}, {salary["link_to_vacancy"]}, {salary["salary_from"]}, {salary["salary_to"]}, '
+                  f'{salary["requirements"]}')
+
+
+hello()
